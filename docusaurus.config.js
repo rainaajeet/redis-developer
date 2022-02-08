fedaaa0c46a25369cacf3958206785250469c553
rainaajeet <98941048+rainaajeet@users.noreply.github.com>
2022-02-08 11:28:19 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@ const path = require('path')
 module.exports = {
   title: 'Redis Developer Hub',
   tagline: 'The Home of Redis Developers',
-  url: 'https://docusaurus-2.netlify.app',
+  url: 'https://docusaurus-v2.netlify.app',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
